0ae5fb8e7bc0100b8acaa44fc03e2d82a94f79b4
Kangbo Lu <redstonegranola@gmail.com>
2018-03-06 16:24:34 -0800

    Simply ProblemA.R qucd() expression

--- ProblemA.R
+++ ProblemA.R
@@ -1,7 +1,7 @@
 # function specifictions ucd family
 ducd <- function(x,c) ifelse(x<=1 || c <= x, 0, (3/(2*(c^1.5-1))) * x^(1/2))
 pucd <- function(q,c) integrate(function(t)(3/(2*(c^1.5-1))) * t^(1/2),1,q)$value
-qucd <- function(q,c) ((q+(1/(c^1.5-1)))*(c^(3/2)-1))^(2/3)
+qucd <- function(q,c) (q*(c^(3/2)-1)+1)^(2/3)
 rucd <- function(n,c) qucd(runif(n),c)
 
 # function checkings

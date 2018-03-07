e7c5c2c32a85244f3aaaee4c8e301aea7933eacb
Kangbo Lu <redstonegranola@gmail.com>
2018-03-06 16:20:11 -0800

    Condense functions specifiction

--- ProblemA.R
+++ ProblemA.R
@@ -1,17 +1,8 @@
-ducd <- function(x,c){
-   ifelse(x<=1 || c <= x, 0, (3/(2*(c^1.5-1))) * x^(1/2))
-}
-pucd <- function(q,c){
-    integrate(function(t)(3/(2*(c^1.5-1))) * t^(1/2),1,q)$value
-    #(q^(3/2))/(c^(3/2)-1)
-}
-qucd <- function(q,c){
-    ((q+(1/(c^1.5-1)))*(c^(3/2)-1))^(2/3)
-}
+# function specifictions ucd family
+ducd <- function(x,c) ifelse(x<=1 || c <= x, 0, (3/(2*(c^1.5-1))) * x^(1/2))
+pucd <- function(q,c) integrate(function(t)(3/(2*(c^1.5-1))) * t^(1/2),1,q)$value
+qucd <- function(q,c) ((q+(1/(c^1.5-1)))*(c^(3/2)-1))^(2/3)
+rucd <- function(n,c) qucd(runif(n),c)
 
-rucd <- function(n,c){
-    tmp <- runif(n)
-    qucd(tmp,c)
-}
-
-mean(rucd(10000000, 5))
+# function checkings
+paste("For c = 5, the mean of X should be about 3.23: ", mean(rucd(10000000, 5)))
